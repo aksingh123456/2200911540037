88d05ce817b0151036afebbb366fed3814004878
Ayush Kumar Singh <ask940555@gmail.com>
2025-09-10 14:19:18 +0530

    my third commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express=require("express")
 const app=express();
-const shortlid=require("shortlid");
+const shortlid=require("shortid");
 app.use(express.json());
 const urlstore={};
+
 app.post("/shortenurl",(req,res)=>{
-    const longurl=req.body;
+    const {url,validity,shortcode}=req.body;
+    console.log("request body",req.body)
     const shorturl=shortlid.generate();
-    urlstore[shorturl]=longurl;
-    res.json({shorturl});
-    
+    urlstore[shorturl]={longurl,expiry};
+    return res.status(201).json({shortLink:` http://localhost:3000${shorturl}`},
+        {expiry:`${expiry}`}
+    )
+
+})
+app.listen(3000,(req,res)=>{
+    console.log("server is running on http://localhost:3000")
 })
