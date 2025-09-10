0ff8fd71d7d7d9ff4f0898486f2e6141ad9a4ea0
Ayush Kumar Singh <ask940555@gmail.com>
2025-09-10 14:27:28 +0530

    my fourth commit

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express=require("express")
 const app=express();
 const shortlid=require("shortid");
 app.use(express.json());
-const urlstore={};
-
 app.post("/shortenurl",(req,res)=>{
     const {url,validity,shortcode}=req.body;
     console.log("request body",req.body)
@@ -12,8 +10,15 @@ app.post("/shortenurl",(req,res)=>{
     return res.status(201).json({shortLink:` http://localhost:3000${shorturl}`},
         {expiry:`${expiry}`}
     )
-
 })
+app.get("/:shortcode",(req,res)=>{
+        const {shortcode}=req.params;
+        return res.json({
+            Description:"Retrive usage statistics",
+            Method:"Get",
+            Route:`${shortcode}`
+        })
+    })
 app.listen(3000,(req,res)=>{
     console.log("server is running on http://localhost:3000")
 })
